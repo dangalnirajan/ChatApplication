243bdb8fbde614af30c02fee79c9c92e35d8d09c
nirajanDangal <98161745+dangalnirajan@users.noreply.github.com>
2022-12-06 15:59:39 +0545

    Add files via upload

--- src/main/java/com/example/chatapplication/config/Config.java
+++ src/main/java/com/example/chatapplication/config/Config.java
@@ -1,4 +1,26 @@
-package com.example.chatapplication.config;/**
+package com.example.chatapplication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
  * @author dangal_nirajan on 05/12/2022
- */public class Config {
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class Config implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/server1").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
 }
--- src/main/java/com/example/chatapplication/controller/MessageController.java
+++ src/main/java/com/example/chatapplication/controller/MessageController.java
@@ -1,4 +1,31 @@
-package com.example.chatapplication.controller;/**
+package com.example.chatapplication.controller;
+
+import com.example.chatapplication.models.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * @author dangal_nirajan on 05/12/2022
- */public class MessageController {
+ */
+
+@RestController
+public class MessageController {
+
+
+    @MessageMapping("/message")
+    @SendTo("/topic/return-to")
+    public Message getContent(@RequestBody Message message) {
+
+//        try {
+//            //processing
+////            Thread.sleep(2000);
+//
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        return message;
+    }
+
 }
